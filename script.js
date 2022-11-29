6e7487a1e53d2771cdcb093263278e0865530f6c
Rodrick <jesprikovic@gmail.com>
2022-11-29 17:54:51 -0500

    Add convert to chess notation function:

--- script.js
+++ script.js
@@ -1,66 +1,35 @@
-const knightsTravailsGraph = {
-    a1: [],
-    b1: [],
-    c1: [],
-    d1: [],
-    e1: [],
-    f1: [],
-    g1: [],
-    h1: [],
-    a2: [],
-    b2: [],
-    c2: [],
-    d2: [],
-    e2: [],
-    f2: [],
-    g2: [],
-    h2: [],
-    a3: [],
-    b3: [],
-    c3: [],
-    d3: [],
-    e3: [],
-    f3: [],
-    g3: [],
-    h3: [],
-    a4: [],
-    b4: [],
-    c4: [],
-    d4: [],
-    e4: [],
-    f4: [],
-    g4: [],
-    h4: [],
-    a5: [],
-    b5: [],
-    c5: [],
-    d5: [],
-    e5: [],
-    f5: [],
-    g5: [],
-    h5: [],
-    a6: [],
-    b6: [],
-    c6: [],
-    d6: [],
-    e6: [],
-    f6: [],
-    g6: [],
-    h6: [],
-    a7: [],
-    b7: [],
-    c7: [],
-    d7: [],
-    e7: [],
-    f7: [],
-    g7: [],
-    h7: [],
-    a8: [],
-    b8: [],
-    c8: [],
-    d8: [],
-    e8: [],
-    f8: [],
-    g8: [],
-    h8: [],
+function convertToChessNotation(coords){
+    const [x, y] = coords;
+    let letter;
+    switch(x) {
+        case 1:
+            letter = 'a';
+            break;
+        case 2:
+            letter = 'b';
+            break;
+        case 3:
+            letter = 'c';
+            break;
+        case 4:
+            letter = 'd';
+            break;
+        case 5:
+            letter = 'e';
+            break;
+        case 6:
+            letter = 'f';
+            break;
+        case 7:
+            letter = 'g';
+            break;
+        case 8:
+            letter = 'h';
+            break;
+    }
+    return letter+y;
+}
+
+function knightMoves(spaceA, spaceB){
+    //    
 }
